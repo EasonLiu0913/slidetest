0fde993004402f40fb66218ff05f4e0f32e9210d
Eason Liu <bettercalleason@gmail.com>
2023-01-19 13:56:51 +0800

    test4

--- src/PerBanner.js
+++ src/PerBanner.js
@@ -4,12 +4,14 @@ const PerBanner = React.forwardRef((props, ref) => {
   const { pathname, banner, distance } = props
   console.log("perBanner", props)
   return (
-    <div style={{ width: "100%", flexShrink: 0 }}>
-      <img
-        src={pathname + banner.img}
-        style={{ width: "100%", transform: `translateX(${distance}px)` }}
-        alt=""
-      />
+    <div
+      style={{
+        width: "100%",
+        flexShrink: 0,
+        transform: `translateX(${distance}px)`,
+      }}
+    >
+      <img src={pathname + banner.img} style={{ width: "100%" }} alt="" />
     </div>
   )
 })
